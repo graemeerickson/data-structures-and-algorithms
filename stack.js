b7c9e72e809b8a2b5a7354bc7f7313956b63e396
graemeerickson <graemeerickson@gmail.com>
2018-12-10 15:06:16 -0800

    Stack data structure with push & pop methods defined

---
+++ stack.js
@@ -0,0 +1,22 @@
+class Stack {
+  constructor() {
+    this.stack = [];
+  }
+
+  push(data) {
+    return this.stack.push();
+  }
+
+  pop() {
+    return this.stack.pop();
+  }
+}
+
+const myStack = new Stack();
+console.log(myStack);
+myStack.push('graeme');
+console.log(myStack);
+myStack.push('erickson');
+console.log(myStack);
+myStack.pop();
+console.log(myStack);
