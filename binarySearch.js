bec47c60ee0ac4fb8a2e9d4d5c2be8d34302cd4a
graemeerickson <graemeerickson@gmail.com>
2018-12-10 13:54:26 -0800

    Binary search recursive implementation

--- binarySearch.js
+++ binarySearch.js
@@ -1,5 +1,7 @@
-// Given an array and a target, return the target's index using binary search
-const binarySearch = (arr, target) => {
+// Given an array and a target integer, return the target's index using binary search
+
+// ITERATIVE APPROACH
+const binarySearchIterative = (arr, target) => {
   let left = 0;
   let right = arr.length - 1;
   while (left <= right) {
@@ -16,7 +18,28 @@ const binarySearch = (arr, target) => {
   return -1;
 }
 
-console.log(binarySearch([1,3,5,8,10], 1));
-console.log(binarySearch([1,3,5,8,10], 5));
-console.log(binarySearch([1,3,5,8,10], 10));
-console.log(binarySearch([1,3,5,8,10], 0));
+console.log(binarySearchIterative([1,3,5,8,10], 1));
+console.log(binarySearchIterative([1,3,5,8,10], 5));
+console.log(binarySearchIterative([1,3,5,8,10], 10));
+console.log(binarySearchIterative([1,3,5,8,10], 0));
+
+// RECURSIVE APPROACH
+const binarySearchRecursive = (arr, target, left = 0, right = arr.length-1) => {
+  let mid = left + Math.floor((right-left) / 2);
+  if (arr[mid] === target) return mid;
+
+  if (arr[mid] < target) {
+    left = mid + 1;
+  } else if (arr[mid] > target) {
+    right = mid - 1;
+  } else {
+    return -1;
+  }
+
+  return binarySearchRecursive(arr, target, left, right);
+}
+
+console.log(binarySearchRecursive([1,3,5,8,10], 1));
+console.log(binarySearchRecursive([1,3,5,8,10], 5));
+console.log(binarySearchRecursive([1,3,5,8,10], 10));
+console.log(binarySearchRecursive([1,3,5,8,10], 0));
